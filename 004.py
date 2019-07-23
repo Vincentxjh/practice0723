75e2ef7be91a5e5a789bb65599bc9efc828db23c
Vincentxjh <53009196+Vincentxjh@users.noreply.github.com>
2019-07-23 11:37:20 +0800

    004
    Simulated Zero-Payment Behavior

---
+++ 004.py
@@ -0,0 +1,9 @@
+#第四个练习-模拟超市抹零结账方式
+
+#给定总金额print，并显示
+money_all = 56.75 + 69.8 + 22.64 + 39.2 + 1.89
+print("商品总额为：" + str(money_all))
+
+#将总金额进行抹零处理，并显示
+money_real = int(money_all)
+print("实收金额为：" + str(money_real))
