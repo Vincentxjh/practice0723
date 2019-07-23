e16f52759b3287676c2f12840b04340e2953375d
Vincentxjh <53009196+Vincentxjh@users.noreply.github.com>
2019-07-23 14:30:36 +0800

    005
    Calculate the difference and average.

---
+++ 005.py
@@ -0,0 +1,12 @@
+#第五个练习-计算分差及平均数
+
+#给定几门课的分数，并显示
+python = 98
+english = 92
+c = 86
+
+#计算分差及求平均数，并显示
+diff = python - c
+aver = (python + english + c)
+print("Python与C语言课程的分差为：" + str(diff) + "分")
+print("三门课的平均分为：" + str(aver) + "分")
