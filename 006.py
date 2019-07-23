8847f3022b2249a434a1c1ae8402ac0f902695fe
Vincentxjh <53009196+Vincentxjh@users.noreply.github.com>
2019-07-23 14:55:57 +0800

    006

---
+++ 006.py
@@ -0,0 +1,17 @@
+#第六个练习-使用比较运算符比较大小
+
+#给定三个变量并赋值,输出变量的值
+python = 98
+english = 92
+c = 87
+print("Python = " + str(python) + " English = " + str(english) + " C = " + str(c))
+
+#使用比较运算符比较它们之间的大小
+print("Python < English ? " + str(python < english))
+print("Python > English ? " + str(python > english))
+print("Python = English ? " + str(python == english))
+print("Python != English ? " + str(python != english))
+print("Python <= C ? " + str(python <= c))
+print("English >= C ? " + str(english >= c))
+
+
